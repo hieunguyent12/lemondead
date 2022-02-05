7a6afbcad17b8cffa1c7eb6ba7ef14876ed2a487
Hieu Nguyen <hieunguyen.0527@gmail.com>
2022-02-05 10:32:52 -0600

    fix

--- packages/ui/AvatarMenu.tsx
+++ packages/ui/AvatarMenu.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-// import SettingsIcon from "../../apps/web/src/icons/Settings";
+import React from "react";
+import SettingsIcon from "../../apps/web/src/icons/Settings";
 
 export type AvatarMenuProps = {};
 
